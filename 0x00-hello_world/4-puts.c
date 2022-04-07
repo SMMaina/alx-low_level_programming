2463dd8e37fe302415dd61eea226333c6c30da3c
SMMaina <mainamuthoni4814@gmail.com>
2022-04-07 10:41:46 -0700

    C code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-# include <stdio.h>
+#include <stdio.h>
 /**
  * main - entry point
  *
- * Return: 0 after printing the function
+ * Return:0 after printing the function
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
