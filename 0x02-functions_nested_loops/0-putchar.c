6eb2931a8969c34c6776a883eecf65d17a91062e
SMMaina <mainamuthoni4814@gmail.com>
2022-04-12 02:54:20 -0700

    First

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include main.h
+/**
+ * main - the entry point
+ *program that prints putchar followed by new line
+ *Return: always return 0 after success
+*/
+
+int main(void)
+{
+	_putchar("_putchar\n");
+	return (0);
+}
