e989f9c53f408c321e91dcc1e9d4139da5055fe0
SMMaina <mainamuthoni4814@gmail.com>
2022-04-12 06:17:08 -0700

    Second

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,18 +8,13 @@
 */
 
 void print_alphabet(void)
-	int main ()
 {
-	int m;
-	m = print_alphabet;
-	return (0);
-}
-{
+
 	int n;
 	for (n = 'a'; n <= 'z'; n++)
 	{
 	 _putchar(n);
-	 return (n);
+	 
 	}
 
 	 _putchar('\n');
