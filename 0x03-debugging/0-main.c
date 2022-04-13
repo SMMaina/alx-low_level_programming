fba09bdd55a83c1102ed2a121870d9c4a1172531
SMMaina <mainamuthoni4814@gmail.com>
2022-04-13 05:20:54 -0700

    First

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,14 +3,13 @@
 
 /**
  * main - Entry point for the function
- * Return: 0 always after success
+ * Return: 0
 */
 int main(void)
 {
 	int i;
 
 	i = 0;
-
 	positive_or_negative(i);
 
 	return (0);
