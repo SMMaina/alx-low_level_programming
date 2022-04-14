f8f8fc6c4bd307a0d883febaf46bf59ff6e2cf71
SMMaina <mainamuthoni4814@gmail.com>
2022-04-14 09:49:15 -0700

    Advanced2

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_number - a function that prints an integer
- * @n: An input integer
- * Return: Nothing
+ * print_number - main function that prints the integer
+ * @n: the number to be printed
+ *
 */
 void print_number(int n)
 {
@@ -14,7 +14,7 @@ void print_number(int n)
 		_putchar('-');
 		num = -num;
 	}
-	
+
 	if ((num / 10) > 0)
 		print_number(num / 10);
 
