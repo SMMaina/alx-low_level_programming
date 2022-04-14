8fb1df200c61556c833dd2e9ddb8330870ea23c8
SMMaina <mainamuthoni4814@gmail.com>
2022-04-14 03:24:01 -0700

    Seventh

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,21 +3,13 @@
  * print_line - prints a straight line
  * @n: defined parameter
 */
-void print_line(int n);
+void print_line(int n)
 {
 	int b;
-	int n = 5;
 
 	for (b = 1; b <= n; b++)
 	{
-		if (b >= 1)
-		{
-			_putchar('_');
-		}
-		else
-		{
-			_putchar('\n');
-		}
+		_putchar('_');
 	}
 	_putchar('\n');
 }
