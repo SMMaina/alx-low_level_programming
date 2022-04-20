2fe672772ce13cd78c9b8d7c5536ba68158eac60
SMMaina <mainamuthoni4814@gmail.com>
2022-04-20 02:36:24 -0700

    Fourth

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - function that prints out a string
+ * @str: defined character parameter
+*/
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,3 +1,4 @@
 creating a pointer
 swapping integer values
 print length of string
+printing string
