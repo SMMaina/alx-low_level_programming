710cc82911e13122cf5587be57e0829bd6e83a40
SMMaina <mainamuthoni4814@gmail.com>
2022-04-26 03:16:15 -0700

    Fourth

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _strspn - a function to return the first occurrence of a string
+ * @s: Scan the strng
+ * @accept: looks for the particular string
+ * Return: an unsigned integer
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, count = 0;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				count++;
+				break;
+			}
+			if (accept[j + 1] == '\0' && s[i] != accept[j])
+			{
+				return (count);
+			}
+		}
+		i++;
+	}
+	return (count);
+}
+
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,3 +1,4 @@
 Using memset function
 copying memory from source string to destination string
 locating a charcater in string
+printing length of a prefix in a substring
