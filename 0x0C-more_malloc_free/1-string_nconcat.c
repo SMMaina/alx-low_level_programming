7b5ba8a01b20822ef3115ae9f01a5a9cc8ac465d
SMMaina <mainamuthoni4814@gmail.com>
2022-05-10 03:43:26 -0700

    Second

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,38 @@
 #include "main.h"
 #include <stdlib.h>
+/**
+ * _strlen1 - function to print length of s1
+ * @s1: character argument
+ * Return: int
+ *
+ */
 
+int _strlen1(char *s1)
+{
+	int a = 0;
+
+	while (s1[a] != '\0')
+	{
+		a++;
+	}
+	return (a);
+}
+
+/**
+ * _strlen2 - function to print length of s2
+ * @s2: character argument
+ * Return: int
+ */
+int _strlen2(char *s2)
+{
+	int b = 0;
+
+	while (s2[b] != '\0')
+	{
+		b++;
+	}
+	return (b);
+}
 /**
  * string_nconcat - function to combine two strings
  * @s1: destination string
@@ -17,8 +49,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	length1 = _strlen(s1);
-	length2 = _strlen(s2);
+	length1 = _strlen1(s1);
+	length2 = _strlen2(s2);
 	if (n > length2)
 		n = length2;
 	size = length1 + n;
@@ -27,7 +59,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	for (i = 0; i < length1; i++)
 		ptr[i] = s1[i];
-	for (j = 0; j < n; j++)
+	for (j = 0; j < n; j++, i++)
 		ptr[i] = s2[j];
 	ptr[i] = 0;
 	return (ptr);
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,6 +6,8 @@
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
+int _strlen1(char *s1);
+int _strlen2(char *s2);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
