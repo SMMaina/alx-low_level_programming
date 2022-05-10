1bdb995d87d9cc411a8c2833633d02f9bc644c9b
SMMaina <mainamuthoni4814@gmail.com>
2022-05-10 06:36:01 -0700

    Advanced2

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,98 +1,28 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-/**
- * _isnum - check if is a number
- * @num: string to check
- * Return: 1 if num, 0 if not
- */
-int _isnum(char *num)
-{
-	int i;
-
-	for (i = 0; num[i] != '\0'; i++)
-	{
-		if (num[i] < '0' || num[i] > '9')
-			return (0);
-	}
-	return (1);
-}
 
 /**
- * _memset - copies a character to the first n characters of the string
- * @s: source string
- * @b: value to replace
- * @n: number of bytes
- * Return: string
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-		s[i] = b;
-	return (s);
-}
-
-/**
- * _strlen - return length of string
- * @s: pointer to character
- * Return: length of string
- */
-int _strlen(char *s)
-{
-	int a = 0;
-
-	while (*(s + a) != '\0')
-		a++;
-	return (a);
-}
-
-/**
- * main - multiplies two positive numbers
+ * main - check the code
  * @argc: number of arguments
- * @argv: contents of the arguments
- * Return: 0 upon succes
+ * @argv: arguments contents
+ * Return: nothing
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int length, c, prod, i, j, l1, l2;
-	int *res;
+	int num1, num2, result;
 
-	if ((argc != 3 || !(_isnum(argv[1]))) || !(_isnum(argv[2])))
-		puts("Error"), exit(98);
-	l1 = _strlen(argv[1]), l2 = _strlen(argv[2]);
-	length = l1 + l2;
-	res = calloc(length, sizeof(int *));
-	if (res == NULL)
-		puts("Error"), exit(98);
-	for (i = l2 - 1; i > -1; i--)
+	if (argc != 3)
 	{
-		c = 0;
-		for (j = l1; j > -1; j--)
-		{
-			prod = (argv[2][i] - '0') * (argv[1][j] - '0');
-			c = (prod / 10);
-			res[(i + j) + 1] += (prod % 10);
-			if (res[(i + j) + 1] > 9)
-			{
-				res[i + j] += res[(i + j) + 1] / 10;
-				res[(i + j) + 1] = res[(i + j) + 1] % 10;
-			}
-			res[(i + j) + 1] += c;
-		}
+		printf("Error\n");
+		exit(98);
 	}
-
-	if (res[0] == 0)
-		i = 1;
 	else
-		i = 0;
-	for (; i < length; i++)
-		printf("%d", res[i]);
-	printf("\n");
-	free(res);
-	return (0);
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		result = num1 * num2;
+		printf("%d\n", result);
+		return (0);
+	}
 }
-
-
