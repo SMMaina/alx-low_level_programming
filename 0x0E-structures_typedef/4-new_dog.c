eb0a7d2b04b00758b3655f6b0bae04139a13cd40
SMMaina <mainamuthoni4814@gmail.com>
2022-05-12 09:24:12 -0700

    Fourth

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -28,6 +28,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dog);
 		return (NULL);
 	}
+	for (i = 0; i < name1; i++)
+		dog->name[i] = name[i];
+
+	dog->age = age;
 	for (owner1 = 0; owner[owner1]; owner1++)
 		;
 	owner1++;
