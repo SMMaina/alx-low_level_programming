1f94afdab1beb249937acd522e188002bad081a0
SMMaina <mainamuthoni4814@gmail.com>
2022-05-16 05:56:57 -0700

    Last

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two integers
+ * @a: first int
+ * @b: second int
+ * Return: sum of two ints
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two ints
+ * @a: first int
+ * @b: second int
+ * Return: diff of two integers
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two ints
+ * @a: first int
+ * @b: second int
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two ints
+ * @a: first int
+ * @b: second int
+ * Return: division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - find the module
+ * @a: first int
+ * @b: second int
+ * Return: module of a
+ */
+int op_mod(int a, int b)
+{
+	if (!b)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
