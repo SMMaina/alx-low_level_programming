0a20b33518328ea9ba6c20e61f9af4f9414fa29a
SMMaina <mainamuthoni4814@gmail.com>
2022-07-11 05:34:39 -0700

    add

---
+++ 0x18-dynamic_libraries/myfunctions.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+/**
+ * add - addition
+ * @x: int first
+ * @y: int second
+ * Return: int
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
+/**
+ * sub - minuses
+ * @x: int first
+ * @y: int secnd
+ * Return: int
+ */
+int sub(int x, int y)
+{
+	return (x - y);
+}
+/**
+ * mul - multiplies
+ * @x: int first
+ * @y: int second
+ * Return: int
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}
+/**
+ * div - checks division
+ * @x: int first
+ * @y: int second
+ * Return: int
+ */
+int div(int x, int y)
+{
+	return (x / y);
+}
+/**
+ * mod - checks module
+ * @x: int first
+ * @y: int next
+ * Return: int
+ */
+int mod(int x, int y)
+{
+	return (x % y);
+}
